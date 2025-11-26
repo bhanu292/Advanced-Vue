794e89855500053b9b237c02b0fdf5934100e70e
bhanu292 <bharat123tiwari123@gmail.com>
2025-11-26 14:24:08 +0530

    Add files via upload

---
+++ c.vue
@@ -0,0 +1,34 @@
+<template>
+<h1>Pass Data to child component</h1>
+<child name="Bharat Tiwari" :user="user" :getData="getData" />
+</template>
+
+<script>
+import child from './child.vue'
+export default {
+    name: "Home",
+    components: {
+        child
+    },
+    data() {
+        return {
+            user: {
+                name: "Alisha",
+                email: 'alisha@gmail.com'
+            }
+        }
+    },
+    methods: {
+        getData() {
+            console.log("parent function called")
+            alert("Hello")
+        }
+    }
+}
+</script>
+
+<style>
+h1 {
+    color: orange;
+}
+</style>
---
+++ child.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2>{{ name }}</h2>
+    <h2>{{ user.name }}</h2> <h2>{{ user.email }}</h2>
+    <button v-on:click="getData()">Call Function</button>
+    </template>
+
+    <script>
+    export default {
+        name: 'child',
+        props: {
+            name: String,
+            user: Object,
+            getData: Function 
+        }
+    }
+</script>
