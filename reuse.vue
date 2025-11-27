a3c4f073b86dd9bb91351ca187a068c25fb8a6c1
bhanu292 <bharat123tiwari123@gmail.com>
2025-11-27 14:05:33 +0530

    Add files via upload

---
+++ reuse.vue
@@ -0,0 +1,52 @@
+<template>
+<h1>Reuse Component</h1>
+<ul>
+    <li v-for="item in Users" :key="item.name">
+
+        <user :data="item"  :getData="getData" />
+    </li>
+
+</ul>
+</template>
+
+<script>
+import user from './user.vue'
+export default {
+    name: "Home",
+    components: {
+        user
+    },
+    methods: {
+        getData (name) {
+            alert(name)
+        }
+    },
+    data() {
+        return {
+            Users: [{
+                    name: "Bhanu",
+                    email: 'bhanu@gmail.com'
+                },
+                {
+                    name: "Nisha",
+                    email: 'nisha@gmail.com'
+                },
+                {
+                    name: "Rohit",
+                    email: 'rohit@gmail.com'
+                },
+                {
+                    name: "Rahul",
+                    email: 'rahul@gmail.com'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+h1 {
+    color: orange;
+}
+</style>
---
+++ user.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="user">
+    <h2>{{ data.name }}</h2>
+    <h2>{{ data.email }}</h2>
+    <button v-on:click="getData(data.name)">Alert</button>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'user',
+    props: {
+        data: Object,
+        getData: Function
+    }
+}
+</script>
+
+<style>
+.user {
+    background-color: skyblue;
+    padding: 10px;
+    border-bottom: 1px solid;
+    margin-bottom: 20px;
+}
+</style>
